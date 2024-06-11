6ea451d87eecd33765b7bcb3694afc10a32addec
fly <2197468432@qq.com>
2024-06-11 19:07:02 +0800

    暂存更改

--- app/pages/gamedetail.tsx
+++ app/pages/gamedetail.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Text, StyleSheet, TouchableOpacity, TextInput } from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity, Image } from "react-native";
 import { useRoute } from "@react-navigation/native";
 import { Ionicons } from "@expo/vector-icons";
 import { useNavigation } from "@react-navigation/native";
@@ -8,6 +8,17 @@ const GameDetail = () => {
     const route = useRoute();
     const { id }: { id: string } = route.params as { id: string };
     const navigation = useNavigation();
+    const result =
+    {
+        id: 1,
+        title: "命运2",
+        score: 9.9,
+        price: 288,
+        image: 'https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/1085660/capsule_sm_120_schinese.jpg?t=1716915649',
+        publisher: 'Bungie',
+        developer: 'Bungie',
+        releaseDate: '2019 年 10 月 1 日',
+    };
 
     return (
         <View style={{ paddingHorizontal: 20, paddingTop: 20, backgroundColor: "#fff", flex: 1 }}>
@@ -16,11 +27,24 @@ const GameDetail = () => {
                     <Ionicons name="arrow-back" size={24} color="black" />
                 </TouchableOpacity>
                 <View style={{ flex: 1, alignItems: 'center', position: 'relative' }}>
-                    <Text style={{ textAlign: 'center', marginLeft: -20 }}>游戏名</Text>
+                    <Text style={{ textAlign: 'center', fontWeight: 'bold', fontSize: 22, marginLeft: -44 }}>{result.title}</Text>
                 </View>
             </View>
-            <Text style={styles.text}>Game Detail {id}</Text>
+            <Image source={{ uri: result.image }} style={{ width: 320, height: 150, alignSelf: 'center', marginBottom: 10, borderRadius: 15 }} />
+
+            <View style={{ flexDirection: 'row', backgroundColor: '#F0F0F0', padding: 10, borderRadius: 8 }}>
+                <View style={{ flex: 1 }}>
+                    <Text>发行商:</Text>
+                    <Text>{result.publisher}</Text>
+                </View>
+                <View style={{ width: 1, backgroundColor: 'black', marginHorizontal: 5 }} />
+                <Text style={{ flex: 1 }}>{result.developer}</Text>
+                <View style={{ width: 1, backgroundColor: 'black', marginHorizontal: 5 }} />
+                <Text style={{ flex: 2 }}>{result.releaseDate}</Text>
+            </View>
         </View>
+
+
     );
 };
 
